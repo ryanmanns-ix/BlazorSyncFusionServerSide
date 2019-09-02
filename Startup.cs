f476d3edbaa82823a6a9815d403f4336025a75b8
Ryan Manns <ryan.manns@imaginexconsulting.com>
2019-09-02 14:37:14 -0400

    Add detail row, export buttons, detailed circuit errors

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace BlazorSyncFusionServerSide
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
             services.AddSingleton<WeatherForecastService>();
         }
 
